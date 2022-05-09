50473b988478dc1b5ebaa5587f470cb9dc13096c
deliretik <deliretik@gmail.com>
2022-05-09 11:16:31 +0100

    firebase setup

--- components/Chat.js
+++ components/Chat.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { View, Text, Platform, KeyboardAvoidingView } from 'react-native';
-
 import { GiftedChat, Bubble, Day, SystemMessage} from 'react-native-gifted-chat';
+
+
+//imorting fierestore
+const firebase = require('firebase');
+require('firebase/firestore');
 
 export default class Chat extends React.Component {
 
@@ -9,107 +13,176 @@ export default class Chat extends React.Component {
     super();
     this.state = {
       messages: [],
+      uid: 0,
+      user: {
+        _id: "",
+        name: "",
+        avatar: ""
     }
+  }
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyCYMEflOC1GMcXNeIn-eFp_uvPbTN3FJ9A",
+    authDomain: "chatapp-c28e2.firebaseapp.com",
+    projectId: "chatapp-c28e2",
+    storageBucket: "chatapp-c28e2.appspot.com",
+    messagingSenderId: "476600146165"
+  }
+
+
+//initialize app 
+  if (!firebase.apps.length){
+    firebase.initializeApp(firebaseConfig);
+  }
+//reference firestore database
+  this.referenceChatMessages = firebase.firestore().collection('messages');
   }
 
 /* componentDidMount function gets called after Chat component mounts. State gets set
 with static message so you see each element of the UI displayed on screen with setState function */
 
-  componentDidMount() {
-    const name = this.props.route.params.name;
+componentDidMount() {
 
-    this.setState({
-      messages: [
-        {
-          _id: 1,
-          text: 'Hello ' + name,
-          createdAt: new Date(),
-          user: {
-            _id: 2,
-            name: 'React Native',
-            avatar: 'https://placeimg.com/140/140/any',
-          },
-        },
-        {
-          _id: 2,
-          text: name + ' has entered the chat',
-          createdAt: new Date(),
-          system: true,
-          fontsColor: '#000'
-        },
-      ],
-    });
+//entered name state from Start screen gets displayed in status bar at the top of the app
+const name = this.props.route.params.name;
+this.props.navigation.setOptions({ title: name});
+
+//authentication
+this.authUnsubscribe = firebase.auth().onAuthStateChanged(async (user) => {
+  if (!user) {
+    await firebase.auth().signInAnonymously();
   }
 
-  onSend(messages = []) {
-    this.setState((previousState) => ({
-      messages: GiftedChat.append(previousState.messages, messages),
-    }));
-  }
+  //update user state with currently active user data
+  this.setState({
+    uid: user.uid,
+    messages: [],
+    user: {
+      _id: user.uid,
+      name: name,
+      avatar: "https://placeimg.com/140/140/any"
+    }
+  });
+  //listener for collection updates
+  this.unsubscribe = this.referenceChatMessages
+    .orderBy('createdAt', 'desc')
+    .onSnapshot(this.onCollectionUpdate);
+  });
+}
 
-  //renderBubble function defines style of user messages
-  renderBubble(props) {
-    return (
-      <Bubble
-        {...props}
-        wrapperStyle={{
-          left: {},
-          right: {},
+onCollectionUpdate = (querySnapshot) => {
+const messages = [];
+
+// go through each document
+querySnapshot.forEach((doc) => {
+
+  // get the QueryDocumentSnapshot's data
+  let data = doc.data();
+  messages.push({
+    _id: data._id,
+    text: data.text,
+    createdAt: data.createdAt.toDate(),
+    user: {
+      _id: data.user._id,
+      name: data.user.name,
+      avatar: data.user.avatar
+    }
+  });
+});
+this.setState({
+  messages: messages
+});
+};
+
+//adding new message to database collection
+addMessage() {
+const message = this.state.messages[0];
+
+this.referenceChatMessages.add({
+  _id: message._id,
+  text: message.text,
+  createdAt: message.createdAt,
+  user: this.state.user
+});
+}
+
+//happens when user sends a message; addMessage() gets called to add message to the state
+onSend(messages = []) {
+this.setState((previousState) => ({
+  messages: GiftedChat.append(previousState.messages, messages),
+}), () => {
+  this.addMessage();
+})
+}
+
+//dont receive updates from collection
+componentWillUnmount() {
+  this.authUnsubscribe();
+  this.unsubscribe();
+}
+
+//renderBubble function defines style of user messages
+renderBubble(props) {
+return (
+  <Bubble
+    {...props}
+    wrapperStyle={{
+      left: {},
+      right: {},
+    }}
+  />
+)
+};
+
+
+//renderDay function renders a message showing the date of the chat; the text color depends on the set background color
+renderDay(props) {
+const { bgColor } = this.props.route.params;
+return (
+  <Day
+    {...props}
+    textStyle= {{color: bgColor === '#B9C6AE' ? '#555555' : '#dddddd'}}
+  />
+);
+}
+
+//renderSystemMessage function renders a system message; the text color depends on the set background color
+renderSystemMessage(props) {
+const { bgColor } = this.props.route.params;
+return (
+  <SystemMessage
+    {...props}
+    textStyle= {{color: bgColor === '#B9C6AE' ? '#555555' : '#dddddd'}}
+  />
+);
+}
+
+render() {
+
+// color picked in Start screen gets applied for chat screen
+const { bgColor } = this.props.route.params;
+
+return (
+  <View style={{
+    flex: 1, 
+    justifyContent: 'center', 
+    backgroundColor: bgColor
+    }}>
+      <GiftedChat 
+        messages={this.state.messages}
+        onSend={(messages) => this.onSend(messages)}
+        renderBubble={this.renderBubble.bind(this)}
+        renderDay={this.renderDay.bind(this)}
+        renderSystemMessage={this.renderSystemMessage.bind(this)}
+        user={{
+          _id: this.state.user._id,
+          name: this.state.name,
+          avatar: this.state.user.avatar
         }}
       />
-    )
-  };
 
-
-  //renderDay function renders a message showing the date of the chat; the text color depends on the set background color
-  renderDay(props) {
-    const { bgColor } = this.props.route.params;
-    return (
-      <Day
-        {...props}
-        textStyle= {{color: bgColor === '#B9C6AE' ? '#555555' : '#dddddd'}}
-      />
-    );
-  }
-
-  //renderSystemMessage function renders a system message; the text color depends on the set background color
-  renderSystemMessage(props) {
-    const { bgColor } = this.props.route.params;
-    return (
-      <SystemMessage
-        {...props}
-        textStyle= {{color: bgColor === '#B9C6AE' ? '#555555' : '#dddddd'}}
-      />
-    );
-  }
-
-  render() {
-    //entered name state from Start screen gets displayed in status bar at the top of the app
-    let name = this.props.route.params.name;
-    this.props.navigation.setOptions({ title: name});
-
-    // color picked in Start screen gets applied for chat screen
-    const { bgColor } = this.props.route.params;
-
-    return (
-      <View style={{
-        flex: 1, 
-        justifyContent: 'center', 
-        backgroundColor: bgColor
-        }}>
-          <GiftedChat 
-            messages={this.state.messages}
-            onSend={(messages) => this.onSend(messages)}
-            renderBubble={this.renderBubble.bind(this)}
-            renderDay={this.renderDay.bind(this)}
-            renderSystemMessage={this.renderSystemMessage.bind(this)}
-            user={{
-              _id: 1,
-            }}
-          />
-
-        { Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null }
-      </View>
-    )
-  }
+    { Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null }
+  </View>
+)
+}
 }
